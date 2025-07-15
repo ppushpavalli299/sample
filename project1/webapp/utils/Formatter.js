f151a7d8da2416eda34bbb6da2468aa768d53a44
Pushpavalli <ppushpavalli299@gmail.com>
2025-07-15 11:25:04 +0530

    implement formatter

---
+++ project1/webapp/utils/Formatter.js
@@ -0,0 +1,20 @@
+sap.ui.define([], () => {
+    return {
+        getStatusText: function (value) {
+            switch (Number(value)) {
+                case 1: return "Draft";
+                case 2: return "Active";
+                case 3: return "Inactive";
+                default: return "Unknown";
+            }
+        },
+        getStatusState: function (value) {
+            switch (Number(value)) {
+                case 1: return "Warning";
+                case 2: return "Success";
+                case 3: return "Error";
+                default: return "None";
+            }
+        }
+    };
+});
